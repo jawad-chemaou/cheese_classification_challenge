d9800f7e3f3cf82b8d11a85d330e91a383272bc5
jawad-chemaou  git config --global user.name jawad-chemaou <chemaouj@gmail.com>
2024-05-18 15:53:24 +0200

    prompt generator llama3 + gpt3.5 simple

---
+++ captions/caption_generator.py
@@ -0,0 +1,26 @@
+from PIL import Image
+import requests
+from transformers import Blip2Processor, Blip2ForConditionalGeneration
+import torch
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
+model = Blip2ForConditionalGeneration.from_pretrained(
+    "Salesforce/blip2-opt-2.7b", load_in_8bit=True, device_map={"": 0}, torch_dtype=torch.float16
+)  # doctest: +IGNORE_RESULT
+
+def generate_caption(label):
+    for i in range(0,25):
+        if i < 10:
+            image = Image.open(f"/users/eleves-b/2022/jawad.chemaou/cheese_classification_challenge/dataset/val/{label}/00000{i}.jpg")
+        else :
+            image = Image.open(f"/users/eleves-b/2022/jawad.chemaou/cheese_classification_challenge/dataset/val/{label}/0000{i}.jpg")
+
+        #prompt = "Question: how many cats are there? Answer:"
+        inputs = processor(images=image, return_tensors="pt").to(device="cuda", dtype=torch.float16)
+
+        generated_ids = model.generate(**inputs)
+        generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
+        
+        return generated_text
